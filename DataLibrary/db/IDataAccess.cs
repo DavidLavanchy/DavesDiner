51f0662d79a25ac0d7805e7ee82334696f3a8540
David Lavanchy <dplavanchy@gmail.com>
2023-07-29 13:10:52 -0400

    Data Access library

---
+++ DataLibrary/db/IDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace DataLibrary.db
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+        Task<int> SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
+    }
+}
